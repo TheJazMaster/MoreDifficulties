baa4082f219407c4693dd01fd44121705449fc6f
Aljaž Perovšek <aljazpero@hotmail.com>
2025-12-06 15:52:17 +0100

    New needed APIs

--- ExternalApis/Essentials.cs
+++ ExternalApis/Essentials.cs
@@ -1,15 +1,12 @@
-﻿using System;
+﻿namespace TheJazMaster.MoreDifficulties;
 
-namespace TheJazMaster.MoreDifficulties;
-
-/// <summary>
-/// Provides access to <c>Nickel.Essentials</c> APIs.
-/// </summary>
 public interface IEssentialsApi
 {
-	/// <summary>The <see cref="UK"/> part of a <see cref="UIKey"/> used by the button that toggles between showing a list of ships on the <see cref="NewRunOptions"/> screen.</summary>
 	UK ShipSelectionToggleUiKey { get; }
 
-	/// <summary>The <see cref="UK"/> part of a <see cref="UIKey"/> used by each button on the list of ships on the <see cref="NewRunOptions"/> screen. The <see cref="UIKey.str"/> will be set to the ship's key.</summary>
 	UK ShipSelectionUiKey { get; }
+
+	bool IsShowingShips { get; }
+	
+	StarterShip? PreviewingShip { get; }
 }
